fba0465588941039949b3b3b98f803d9b798ceb7
mcarvallom <marcocarvallo1303@gmail.com>
2025-04-12 19:10:28 -0400

    menuLateral

---
+++ gymhub/lib/marco/inicio/inicio.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gymhub/marco/componentes/menu_lateral.dart';
+class Inicio extends StatelessWidget {
+  const Inicio({Key? key}) : super(key: key);
+/*/Users/mcarvallom/flutter/bin/flutter run*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Container(
+        decoration: BoxDecoration(
+        color: Color(0xFF1F2836),
+        border: Border.all(
+          color: const Color.fromARGB(100, 205, 205, 205),
+        ),
+      ),
+        child: Center(
+          child: Row(
+            children: [
+              MenuLateralWidget(currentRoute: '/inicio'),
+              Column(
+                children: [
+                  Text("Hola, GymHub!", style: TextStyle(color: Colors.white)),
+                  SizedBox(height: 16),
+                  Text("Esta es la pantalla de inicio", style: TextStyle(color: Colors.white)),
+                ],
+              )
+            ]
+          ),
+        ),
+      ),
+    );}}
---
+++ gymhub/lib/marco/usuarios.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:gymhub/marco/componentes/menu_lateral.dart';
+class Usuarios extends StatelessWidget {
+  const Usuarios({Key? key}) : super(key: key);
+/*/Users/mcarvallom/flutter/bin/flutter run*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Container(
+        decoration: BoxDecoration(
+        color: Color(0xFF1F2836),
+        border: Border.all(
+          color: const Color.fromARGB(100, 205, 205, 205),
+        ),
+      ),
+        child: Center(
+          child: Row(
+            children: [
+              MenuLateralWidget(currentRoute: '/usuarios'),
+              Column(
+                children: [
+                  Text("Hola, GymHub!", style: TextStyle(color: Colors.white)),
+                  SizedBox(height: 16),
+                  Text("Esta es la pantalla de usuarios", style: TextStyle(color: Colors.white)),
+                ],
+              )
+            ]
+          ),
+        ),
+      ),
+    );}}
