1cd82cf3ee1c3928dd32c9342646175d1faedec4
nicolasAG2 <ni.gonzalez2@duocuc.cl>
2025-04-14 01:01:34 -0400

    se agrega tabla para los planes

--- gymhub/lib/presentation/pages/planes/planes.dart
+++ gymhub/lib/presentation/pages/planes/planes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gymhub/presentation/widgets/menu_responsive.dart';
 
+
 class Planes extends StatefulWidget {
 const Planes({super.key});
 
@@ -13,9 +14,33 @@ class _PlanesState extends State<Planes> {
 Widget build(BuildContext context) {
 return ResponsiveScaffold(
     currentRoute: '/planes',
-    body: Center(
-    child: Text('Contenido principal aquí'),
-),
-);
-}
+    body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal, // Por si hay muchas columnas
+        child: DataTable(
+            columns: [
+            DataColumn(label: Text('ID')),
+            DataColumn(label: Text('Nombre')),
+            DataColumn(label: Text('Edad')),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text('1')),
+              DataCell(Text('Juan')),
+              DataCell(Text('25')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('2')),
+              DataCell(Text('Ana')),
+              DataCell(Text('30')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('3')),
+              DataCell(Text('Carlos')),
+              DataCell(Text('28')),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
 }
